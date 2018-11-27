57b9e336cd9579158a1bc452cfdb2a3bae8fbe88
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-27 16:52:46 +0100

    menu responsive funzionante sia in apertura che chiusura ma troppe righe

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var buttonResponsive = $('#btnMR');
+var buttonResponsiveClose = $('.btnClose');
 var containerMenu = $('.wrapMenu');
 var nav = $('.menuNav');
 
@@ -9,5 +10,26 @@ buttonResponsive.click(function(){
   containerMenu.addClass('apriMenu');
 });
 buttonResponsive.click(function(){
+  nav.removeClass('menuNav');
+});
+buttonResponsive.click(function(){
   nav.addClass('menuNavHamuberger');
+});
+buttonResponsive.click(function(){
+  buttonResponsiveClose.removeClass('btnClose');
+});
+buttonResponsive.click(function(){
+  buttonResponsiveClose.addClass('btnCloseOpen');
+});
+buttonResponsiveClose.click(function(){
+  containerMenu.removeClass('apriMenu');
+});
+buttonResponsiveClose.click(function(){
+  containerMenu.addClass('wrapMenu');
+});
+buttonResponsiveClose.click(function(){
+  buttonResponsiveClose.removeClass('btnCloseOpen');
+});
+buttonResponsiveClose.click(function(){
+  buttonResponsiveClose.addClass('btnClose');
 });
